ea3bbb6d1503babd08737bbaa2ac638224dc61ef
duc123456 <kygame147@gmail.com>
2023-01-31 16:34:00 +0700

    Project

---
+++ SWP301/src/java/dal/DAO.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.User;
+
+/**
+ *
+ * @author nhant
+ */
+public class DAO extends DBContext{
+    
+    
+    //check login xem co dung khong
+    public User login(String user, String pass) {
+
+        String query = "Select * from [User] where  [username]  =?\n"
+                + "  and [password] =?";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, user);
+            st.setString(2, pass);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                return new User(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getBoolean(10),rs.getString(11), rs.getString(12)); 
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    //check
+//    public User check(String username, String password) {
+//
+//        String sql = "Select * from [User] where  [username]  =?\n"
+//                + "  and [password] =?";
+//        try {
+//            PreparedStatement st = connection.prepareStatement(sql);
+//            st.setString(1, username);
+//            st.setString(2, password);
+//            ResultSet rs = st.executeQuery();
+//            if (rs.next()) {
+//                User u = new User();
+//                u.setUsername(username);
+//                u.setPass(password);
+//                return u;
+//                
+//            }
+//        } catch (SQLException e) {
+//            System.out.println(e);
+//        }
+//
+//        return null;
+//    }
+
+    //check user co ton tai khong
+//    public User checkUserExist(String user) {
+//        String query = "Select * from [User] where  [user] = ?";
+//
+//        try {
+//            PreparedStatement st = connection.prepareStatement(query);
+//            st.setString(1, user);
+//            ResultSet rs = st.executeQuery();
+//            while (rs.next()) {
+//                return new User(rs.getInt(1),
+//                        rs.getString(2),
+//                        rs.getString(3),
+//                        rs.getInt(4),
+//                        rs.getString(5),
+//                        rs.getString(6),
+//                        rs.getString(7),
+//                        rs.getString(8));
+//
+//            }
+//        } catch (SQLException e) {
+//
+//        }
+//        return null;
+//    }
+//
+//    //dang ki tai khoan
+//    public void signup(String user, String pass, String fullname, String email, String phonenumber, String address) {
+//        String query = "insert into [User]\n"
+//                + "values(?,?,0,?,?,?,?)";
+//        try {
+//            PreparedStatement st = connection.prepareStatement(query);
+//            st.setString(1, user);
+//            st.setString(2, pass);
+//            st.setString(3, fullname);
+//            st.setString(4, email);
+//            st.setString(5, phonenumber);
+//            st.setString(6, address);
+//            st.executeUpdate();
+//        } catch (SQLException e) {
+//
+//        }
+//    }
+    public static void main(String[] args) {
+        DAO d = new DAO();
+        User u = d.login("a", "123");
+        System.out.println(u.getEmail());
+    }
+
+    
+}
---
+++ SWP301/src/java/model/User.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nhant
+ */
+public class User {
+
+   private int uId, roleId;
+   private String fName, lName, username, pass, address, image;
+   private String dob;
+   private boolean gender;
+   private String phone, email;
+
+    public User() {
+    }
+
+    public User(int uId, int roleId, String fName, String lName, String username, String pass, String address, String image, String dob, boolean gender, String phone, String email) {
+        this.uId = uId;
+        this.roleId = roleId;
+        this.fName = fName;
+        this.lName = lName;
+        this.username = username;
+        this.pass = pass;
+        this.address = address;
+        this.image = image;
+        this.dob = dob;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getuId() {
+        return uId;
+    }
+
+    public void setuId(int uId) {
+        this.uId = uId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   
+}
